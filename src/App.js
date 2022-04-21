aba867afa7a97447d8ab25a35a7e30051945f802
abdec2 <abdec2@hotmail.com>
2022-04-21 21:09:14 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import HeaderComponent from "./components/Header";
+import Presale from "./components/Presale";
+import ShapeDivider from "./components/ShapeDivider";
+import FooterComponent from './components/Footer';
+import { useContext, useEffect } from "react";
+import { GlobalProvider } from "./context/GlobalContext";
+
+function App() {
+  useEffect(() => {
+    if (!window.ethereum) {
+      alert('Please install MetaMask');
+    }
+
+  }, []);
+  return (
+    <GlobalProvider>
+      <div className="container mx-auto px-10 max-w-7xl">
+        <div className="min-h-screen">
+          <HeaderComponent />
+          {/* <ShapeDivider /> */}
+          <Presale />
+        </div>
+      </div>
+      <FooterComponent />
+    </GlobalProvider>
+  );
+}
+
+export default App;
